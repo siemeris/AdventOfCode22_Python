f0ba72fe65434f491f32da9a8e9f2334af8778de
siemeris <siemeris@gmail.com>
2022-12-03 17:35:09 +0000

    Day3 Parte 3

--- Day3/solution.py
+++ Day3/solution.py
@@ -7,7 +7,6 @@ def pesoLetra(letra):
     mayus = list(map(chr, range(65, 91)))
     alfabeto = minus + mayus
     peso = alfabeto.index(letra)
-    print(peso, "peso")
     return peso+1
 
 pesoTotal=0
@@ -17,9 +16,29 @@ for line in lines:
     for letraEn1 in line[0:m]:
         for letraEn2 in line[m:]:
             if letraEn1==letraEn2 and letraEn1!=letraEncontrada:
-                print(letraEn1, letraEncontrada, "hola?")
                 letraEncontrada=letraEn1
                 pesoTotal+= pesoLetra(letraEn1)
 print(pesoTotal)
+#Resultado PARTE1 = 8053
 
 #PART2
+pesoTotalParte2=0
+for idx, line in enumerate(lines):
+    letras=''
+    letraEncontradaParte2=''
+    if idx==0 or idx%3==0:
+        # print(lines[idx],lines[idx+1],lines[idx+2])
+        for letra1 in lines[idx]:
+            for letra2 in lines[idx+1]:
+                if letra1==letra2 and letra1 !='\n':
+                    letras+=letra1
+            for letra3 in lines[idx+2]:
+                if letras.find(letra3)!=-1 and letra3!=letraEncontradaParte2:
+                    letraEncontradaParte2=letra3
+                    pesoTotalParte2+=pesoLetra(letraEncontradaParte2)
+                    # print(letraEncontradaParte2, "ENCONTRADA")
+print(pesoTotalParte2)
+#Resultado PARTE2 = 2425
+
+
+
